205598ad7c2d93667ab9584959c13b688e7232da
Ermal Bujupaj <ermal.bj@gmail.com>
2026-01-30 22:54:01 +0100

    temporarily remove test target

--- Package.swift
+++ Package.swift
@@ -21,9 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftUIDesignSystem"
-        ),
-        .testTarget(
-            name: "SwiftUIDesignSystemTests",
-            dependencies: ["SwiftUIDesignSystem"])
+        )
     ]
 )
